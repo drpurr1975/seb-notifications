0b9c627d67c52a0064c57876e2df9c4a63fa7be7
Mikhail Kurenkov <dr.purr@gmail.com>
2020-07-28 11:45:31 +0600

    fix token issue

--- sebl_warn.py
+++ sebl_warn.py
@@ -1,64 +1,17 @@
-#!/usr/bin/python3.8git
+#!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import requests, datetime, re, json, telebot, ssl, config
-from aiohttp import web
+import requests, datetime, re, json, os, time, sys
+import telebot
+from flask import Flask
+import config
 
-WEBHOOK_LISTEN = "0.0.0.0"
-WEBHOOK_PORT = 8443
+#bot = telebot.TeleBot(API_TOKEN)
 
-WEBHOOK_SSL_CERT = "/etc/letsencrypt/live/YOUR.DOMAIN/fullchain.pem"
-WEBHOOK_SSL_PRIV = "/etc/letsencrypt/live/YOUR.DOMAIN/privkey.pem"
-
-API_TOKEN = '968634485:AAER3wucMp_6YWYac_tnqSqtoGSV8V_4fMw'
-bot = TeleBot(API_TOKEN)
-
-app = web.Application()
-
-# process only requests with correct bot token
-async def handle(request):
-    if request.match_info.get("token") == bot.token:
-        request_body_dict = await request.json()
-        update = telebot.types.Update.de_json(request_body_dict)
-        bot.process_new_updates([update])
-        return web.Response()
-    else:
-        return web.Response(status=403)
-
-app.router.add_post("/{token}/", handle)
-
-help_string = []
-help_string.append("*Some bot* - just a bot.\n\n")
-help_string.append("/start - greetings\n")
-help_string.append("/help - shows this help")
-
-# - - - messages
-
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Ololo, I am a bot")
-
-@bot.message_handler(commands=["help"])
-def send_help(message):
-    bot.send_message(message.chat.id, "".join(help_string), parse_mode="Markdown")
-
-# - - -
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
-
-# start aiohttp server (our bot)
-web.run_app(
-    app,
-    host=WEBHOOK_LISTEN,
-    port=WEBHOOK_PORT,
-    ssl_context=context,
-)
 
 def telegram_bot_sendtext(id, bot_message):
-    bot_token = '968634485:AAER3wucMp_6YWYac_tnqSqtoGSV8V_4fMw'
-    seb_bot = telebot.TeleBot(bot_token)
+    seb_bot = telebot.TeleBot(API_TOKEN)
     seb_bot.send_message(chat_id=id, text=bot_message, parse_mode='HTML')
 #    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + id + '&parse_mode=MarkdownV2&text=' + bot_message
 
